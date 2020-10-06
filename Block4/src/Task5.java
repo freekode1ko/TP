67d1fd8ccbd7499159347e77fcb9b2d23774b720
Freekode1ko <Freekode1ko@gmail.com>
2020-10-06 21:41:31 +0300

    Edited task 5
    now corrected version

--- Block4/src/Task5.java
+++ Block4/src/Task5.java
@@ -2,24 +2,33 @@ public class Task5
 {
 	public static void main(String[] args) 
 	{
-		System.out.println(BMI("205 pounds", "73 inches"));
+		System.out.println(BMI("154 pounds", "2 meters"));
 	}
 
-	private static double BMI(String Mass, String Height) 
+	private static String BMI(String Mass, String Height) 
 	{
 		String[] MassParse = Mass.split(" ");
 		String[] HeightParse = Height.split(" ");
 		double mass;
 		double height;
 		
-		if(MassParse[1]=="pounds")
+		if(MassParse[1].equals("pounds"))
 			mass = Double.parseDouble(MassParse[0])*0.453592;
 		else mass = Double.parseDouble(MassParse[0]);
 		
-		if(HeightParse[1]=="inches")
-			height = Double.parseDouble(HeightParse[0])*2.54;
+		if(HeightParse[1].equals("inches"))
+			height = Double.parseDouble(HeightParse[0])/39.37;
 		else height = Double.parseDouble(HeightParse[0]);
-
-		return (double)Math.round(Math.pow((mass/2.205)/(height/39.37),2)*100)/100;
+		
+		double bmi = (double)Math.round(mass/(Math.pow(height, 2))*100)/100;
+		
+		if (bmi < 18.5)
+			return bmi+ " Underweight";
+		else if (18.5 <= bmi && bmi <= 24.9)
+			return bmi+ " Normal weight";
+		else if (bmi >= 25)
+			return bmi+ " Overweight";
+		
+		return "Wrong data at input";
 	}
 }
